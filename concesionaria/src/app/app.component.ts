fcf97d0b21341e6b6c20d6a0b84db445cf343e51
Guille-B <guillebau2012@gmail.com>
2024-10-03 17:23:30 -0300

    cambio rutas

--- concesionaria/src/app/app.component.ts
+++ concesionaria/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MenuComponent],
+  imports: [RouterOutlet,MenuComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- concesionaria/src/app/app.module.ts
+++ concesionaria/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutes } from './app.routes';
+import { AppRoutes, routes } from './app.routes';
 import { MenuComponent } from './menu/menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { InicioComponent } from './inicio/inicio.component';
 import { AppComponent } from './app.component';
 import {MaterialModule} from  './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { provideRouter } from '@angular/router';
 @NgModule({
     declarations: [
       AppComponent,
@@ -25,7 +25,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       MaterialModule,
       BrowserAnimationsModule
     ],
-    providers: [],
+    providers: [provideRouter(routes)],
     bootstrap: [AppComponent]
   })
   export class AppModule { }
